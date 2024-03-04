6c17e817f1bbd2e5161f63ea1e22256f1250b9d5
Dario Zerpa <dariojze@gmail.com>
2024-03-04 18:16:46 +0000

    Data convertida, html creados, app hecha

--- src/app.py
+++ src/app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+import pickle
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# Cargar el modelo, el vectorizador y la data desde el archivo .sav
+
+with open("/workspace/flask-deploy-ml-excercise/models/knn_neighbors-6_algorithm-brute_metric-cosine.sav", "rb") as f:
+    knn_model = pickle.load(f)
+    
+with open("/workspace/flask-deploy-ml-excercise/data/processed/total_data.sav", "rb") as f:
+    total_data = pickle.load(f)
+    
+vectorizer = TfidfVectorizer(token_pattern=r'\b\w+\b', lowercase=True)
+vectorizer.fit(total_data['tags'])
+    
+def recommend_similar_movies(movie_title):
+    input_vector = vectorizer.transform([movie_title])
+    distances, indices = knn_model.kneighbors(input_vector)
+    recommended_movies = [(total_data["title"][i], distances[0][j]) for j, i in enumerate(indices[0])]
+    return recommended_movies
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+    movie_title = request.form['movie-title']
+    recommended_movies = recommend_similar_movies(movie_title)
+    return render_template('recommendations.html', movie_title=movie_title, recommended_movies=recommended_movies)
+
+if __name__ == '__main__':
+    app.run(debug=True)
